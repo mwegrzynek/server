66d0ba7de80b6613dc3a69785b7011e32812f094
Joas Schilling <coding@schilljs.com>
2019-12-05 14:38:28 +0100

    Pick a shorter name for the transfer ownership table
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- apps/files/lib/Migration/Version11301Date20191205150729.php
+++ apps/files/lib/Migration/Version11301Date20191205150729.php
@@ -22,15 +22,17 @@ declare(strict_types=1);
  *
  */
 
-
 namespace OCA\Files\Migration;
 
 use Closure;
 use OCP\DB\ISchemaWrapper;
-use OCP\Migration\SimpleMigrationStep;
 use OCP\Migration\IOutput;
+use OCP\Migration\SimpleMigrationStep;
 
-class Version11301Date20191113195931 extends SimpleMigrationStep {
+/**
+ * Auto-generated migration step: Please modify to your needs!
+ */
+class Version11301Date20191205150729 extends SimpleMigrationStep {
 
 	/**
 	 * @param IOutput $output
@@ -42,13 +44,12 @@ class Version11301Date20191113195931 extends SimpleMigrationStep {
 		/** @var ISchemaWrapper $schema */
 		$schema = $schemaClosure();
 
-		/*
-		 * Disabled because it breaks installing on Oracle
-		$table = $schema->createTable('user_transfer_ownership');
-		$table->addColumn('id', 'integer', [
+		$table = $schema->createTable('user_transfer_owner');
+		$table->addColumn('id', 'bigint', [
 			'autoincrement' => true,
 			'notnull' => true,
-			'length' => 4,
+			'length' => 20,
+			'unsigned' => true,
 		]);
 		$table->addColumn('source_user', 'string', [
 			'notnull' => true,
@@ -68,8 +69,12 @@ class Version11301Date20191113195931 extends SimpleMigrationStep {
 		]);
 		$table->setPrimaryKey(['id']);
 
-		return $schema;
-		 */
-	}
+		// Quite radical, we just assume no one updates cross beta with a pending request.
+		// Do not try this at home
+		if ($schema->hasTable('user_transfer_ownership')) {
+			$schema->dropTable('user_transfer_ownership');
+		}
 
+		return $schema;
+	}
 }
